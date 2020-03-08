cb6881b0014126b04a42ba0b2d0023404cd387b4
Dargan <mpdargan@uiowa.edu>
2020-03-07 18:57:59 -0600

    Fix preprocessing and add label MinMaxScaler

--- model.py
+++ model.py
@@ -1,39 +1,64 @@
 """LSTM model used for stock prediction."""
+from pandas import DataFrame
 from preprocess import preprocess
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, LSTM
+from tensorflow.keras.layers import Dense, Dropout, LSTM
 
+BATCH_SIZE = 64
 EPOCHS = 50
+
+stock_data, next_day_close_labels, stock_scaler, label_scaler = preprocess(
+    "./data/MCD.csv"
+)
+hahahaha = stock_data.copy()
+stock_data = stock_data.to_numpy().reshape(stock_data.shape[0], 1, stock_data.shape[1])
+stock_train, stock_test, label_train, label_test = train_test_split(
+    stock_data, next_day_close_labels, test_size=0.3, shuffle=False
+)
+
+# train_dataset = (
+#     tf.data.Dataset.from_tensor_slices((stock_train, label_train))
+#     # .shuffle(len(stock_train))
+#     .batch(BATCH_SIZE).repeat()
+# )
+# test_dataset = (
+#     tf.data.Dataset.from_tensor_slices((stock_test, label_test))
+#     # .shuffle(len(stock_test))
+#     .batch(BATCH_SIZE).repeat()
+# )
 
 model = Sequential(
     [
-        LSTM(10, input_shape=(23,)),
-        Dense(128, activation="gelu"),
-        Dense(64, activation="gelu"),
-        Dense(10, activation="gelu"),
-        Dense(2, activation="sigmoid"),
+        LSTM(50, return_sequences=True, input_shape=stock_data.shape[1:]),
+        Dropout(0.2),
+        LSTM(50, return_sequences=True),
+        Dropout(0.2),
+        LSTM(50, return_sequences=True),
+        Dropout(0.2),
+        LSTM(50),
+        Dropout(0.2),
+        Dense(1),
     ]
 )
-model.compile(optimizer="adam", loss="sparse_categorical_crossentropy")
+model.compile(optimizer="adam", loss="mse", metrics=["accuracy"])
 
 if __name__ == "__main__":
-    stock_data, next_day_close_labels, scaler = preprocess("./data/MCD.csv")
-    stock_train, stock_test, label_train, label_test = train_test_split(
-        stock_data, next_day_close_labels, test_size=0.3, shuffle=False
-    )
-
-    train_dataset = tf.data.Dataset.from_tensor_slices(
-        (stock_train.values, label_train)
-    ).batch(32)
-    test_dataset = tf.data.Dataset.from_tensor_slices(
-        (stock_test.values, label_test)
-    ).batch(32)
-
     model.fit(
-        train_dataset, epochs=EPOCHS, validation_data=test_dataset,
+        stock_train,
+        label_train,
+        epochs=EPOCHS,
+        # steps_per_epoch=len(stock_train),
+        validation_data=(stock_test, label_test),
+        # validation_steps=len(stock_test),
     )
+
+    # TODO look into which columns are which, might be why mae is so high
+    pred = model.predict(stock_test)
+    pred = label_scaler.inverse_transform(pred)
+    hahahaha = DataFrame(stock_scaler.inverse_transform(hahahaha))
+    print(f"test: {hahahaha}, pred: {pred}")
 
     # Denormalize data back to original scale
     # scaler.inverse_transform(stock_data)
--- preprocess.py
+++ preprocess.py
@@ -8,7 +8,7 @@ from ta.volatility import average_true_range, bollinger_mavg
 from typing import Tuple
 
 
-def preprocess(csv_path: str) -> Tuple[DataFrame, Series, MinMaxScaler]:
+def preprocess(csv_path: str) -> Tuple[DataFrame, Series, MinMaxScaler, MinMaxScaler]:
     """
     Preprocess stock market data to prepare it to be used as training and testing
     data for the model.
@@ -32,25 +32,26 @@ def preprocess(csv_path: str) -> Tuple[DataFrame, Series, MinMaxScaler]:
     df["low"] = df["low"].str.replace("$", "").astype(np.float64)
     df["close"] = df["close"].str.replace("$", "").astype(np.float64)
 
-    compute_indicators(df)
+    # compute_indicators(df)
 
     # Add indices to include information of the larger market movement
     add_spx(df)
     add_vix(df)
 
-    df.dropna(inplace=True)
-
-    # Normalize datapoints between 0 and 1
-    scaler = normalize(df)
-
     # Convert timestamp column to timestamp datatype and set it the as index
     df["timestamp"] = to_datetime(df["timestamp"])
     df.set_index("timestamp", inplace=True)
 
+    df.dropna(inplace=True)
+
     # Get labels for each row in the dataframe
     labels = get_labels(df)
 
-    return df, labels, scaler
+    # Normalize datapoints between 0 and 1
+    df, df_scaler = normalize_dataframe(df)
+    labels, label_scaler = normalize_labels(labels)
+
+    return df, labels, df_scaler, label_scaler
 
 
 def compute_indicators(df: DataFrame) -> DataFrame:
@@ -62,44 +63,9 @@ def compute_indicators(df: DataFrame) -> DataFrame:
     df["boll"] = bollinger_mavg(df["close"])
     df["roc"] = roc(df["close"])
     df["wpr"] = wr(df["high"], df["low"], df["close"])
-    df["ema50"] = ema_indicator(df["close"], 50)
-    df["ema100"] = ema_indicator(df["close"], 100)
-    df["ema200"] = ema_indicator(df["close"], 200)
-
-
-def normalize(df: DataFrame) -> MinMaxScaler:
-    """Normalize dataframe to be used inside the neural network."""
-    columns = [
-        "close",
-        "volume",
-        "open",
-        "high",
-        "low",
-        "rsi14",
-        "macd",
-        "cci",
-        "atr",
-        "boll",
-        "roc",
-        "wpr",
-        "ema50",
-        "ema100",
-        "ema200",
-        "spx_open",
-        "spx_high",
-        "spx_low",
-        "spx_close",
-        "vix_open",
-        "vix_high",
-        "vix_low",
-        "vix_close",
-    ]
-    values = df[columns].values
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    values = scaler.fit_transform(values)
-    df[columns] = values
-
-    return scaler
+    # df["ema50"] = ema_indicator(df["close"], 50)
+    # df["ema100"] = ema_indicator(df["close"], 100)
+    # df["ema200"] = ema_indicator(df["close"], 200)
 
 
 def add_spx(df: DataFrame) -> None:
@@ -120,6 +86,16 @@ def add_vix(df: DataFrame) -> None:
     df["vix_close"] = vix_data["Close"].astype(np.float64)
 
 
+def normalize_dataframe(df: DataFrame) -> Tuple[DataFrame, MinMaxScaler]:
+    """Normalize dataframe values."""
+    values = df.values
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    values = scaler.fit_transform(values)
+    df = DataFrame(values)
+
+    return df, scaler
+
+
 def get_labels(df: DataFrame) -> Series:
     """Get labels for a dataframe (closing values from the next day)."""
     closing_values = df["close"].values
@@ -129,6 +105,13 @@ def get_labels(df: DataFrame) -> Series:
     df.drop(df.tail(1).index, inplace=True)
 
     return closing_values
+
+
+def normalize_labels(labels: Series) -> Tuple[Series, MinMaxScaler]:
+    """Normalize label values."""
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    labels = scaler.fit_transform(labels.reshape(-1, 1))
+    return labels, scaler
 
 
 if __name__ == "__main__":
